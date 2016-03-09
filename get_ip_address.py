d85f871071ad218dd6f95689449d63cfabe42d2e
HJain99 <hjcodes@gmail.com>
2016-03-09 06:41:56 +0530

    Added program to fetch IP
    Fetches the IP address of the site using ping command

---
+++ get_ip_address.py
@@ -0,0 +1,11 @@
+import os
+
+def get_ip_address(url):
+    command = 'ping ' + url
+    process = os.popen(command)
+    results = str(process.read())
+    startmarker = results.find('[')
+    endmarker = results.find(']')
+    return results[startmarker+1:endmarker]
+
+print(get_ip_address('flipkart.com'))
